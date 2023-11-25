ca549b5a468380b95eaa68c41a61e8d3a13b2546
pramila <pramila@dbcyelagiri.edu.in>
2023-11-25 10:14:22 +0530

    Mannual adjustment

--- src/app/pages/timetables/manual-adjustment/manual-adjustment.component.ts
+++ src/app/pages/timetables/manual-adjustment/manual-adjustment.component.ts
@@ -16,6 +16,7 @@ export class ManualAdjustmentComponent implements OnInit {
   NoOfDays: any;
   NoOfPeriods: any;
 
+  yourDataArray: string[][] = [];
   ngOnInit(): void {
     this.getNonFixPeriodsList();
     this.getManualAdjustmentPeriodsList();
@@ -71,9 +72,16 @@ export class ManualAdjustmentComponent implements OnInit {
 
   getSubject(timetableData: string, rowIndex: number, colIndex: number): string {
     const dayData = timetableData.split("#")[rowIndex] ? timetableData.split("#")[rowIndex].split("@") : [];
-    return colIndex < dayData.length ? dayData[colIndex] : '';
+    return (colIndex < dayData.length && (dayData[colIndex] != '' && dayData[colIndex] != '0')) ? dayData[colIndex]  : dayData[colIndex] == '0' ? '' : 'Break';
   }
-
+  
+  shouldDisableDrag(rowIndex: number, colIndex: number, colValue: any): boolean {
+    return (colIndex == 0 || colValue == 'Break')? true : false; // Dragging is enabled by default
+  }
+//  onDrop(event: CdkDragDrop<string[][]>): void {
+//   const { container, previousIndex, currentIndex } = event;
+//   moveItemInArray(container.data, previousIndex, currentIndex);
+// }
   // numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
   // day1 = [{ name: 'monday', 'disabled': true, 'color': 'background-color:#4d4fc1d1; color: #fff;' },
@@ -138,23 +146,23 @@ export class ManualAdjustmentComponent implements OnInit {
   // { name: 'Social', 'disabled': false },
   // { name: 'Science', 'disabled': false }];
 
-  // drop(event: CdkDragDrop<any[]>) {
-  //   console.log(event.container.data, event.currentIndex, event.previousContainer.data, event.previousIndex);
-  //   let currIndex = event.currentIndex != 9 ? event.currentIndex : 8;
-  //   let preIndex = event.previousIndex != 9 ? event.previousIndex : 8;
-  //   let temp = event.container.data[currIndex]
+  onDrop(event: CdkDragDrop<any[]>) {
+    console.log(event.container.data, event.currentIndex, event.previousContainer.data, event.previousIndex);
+    let currIndex = event.currentIndex != 9 ? event.currentIndex : 8;
+    let preIndex = event.previousIndex != 9 ? event.previousIndex : 8;
+    let temp = event.container.data[currIndex]
 
-  //   if (![0, 3, 5].includes(currIndex)) {
-  //     if (event.previousContainer === event.container) {
-  //       event.container.data.splice(currIndex, 1, event.container.data[preIndex]);
-  //       event.container.data.splice(preIndex, 1, temp);
+    if (![0, 3, 5].includes(currIndex)) {
+      if (event.previousContainer === event.container) {
+        event.container.data.splice(currIndex, 1, event.container.data[preIndex]);
+        event.container.data.splice(preIndex, 1, temp);
 
-  //     } else {
-  //       event.container.data.splice(currIndex, 1, event.previousContainer.data[preIndex]);
-  //       event.previousContainer.data.splice(preIndex, 1, temp);
-  //     }
-  //   } 
-  // }
+      } else {
+        event.container.data.splice(currIndex, 1, event.previousContainer.data[preIndex]);
+        event.previousContainer.data.splice(preIndex, 1, temp);
+      }
+    } 
+  }
 
 }
 
--- src/app/pages/timetables/timetables.component.ts
+++ src/app/pages/timetables/timetables.component.ts
@@ -7,6 +7,7 @@ import Swal from 'sweetalert2';
 declare var $: any; // Declare jQuery globally (not recommended, but works for demonstration)
 import { saveAs } from 'file-saver';
 import * as XLSX from 'xlsx';
+import * as ExcelJS from 'exceljs';
 @Component({
   selector: 'app-timetables',
   templateUrl: './timetables.component.html',
@@ -113,7 +114,8 @@ export class TimetablesComponent implements OnInit {
     this.service.getHttpServiceWithDynamicParams(postData, 'ViewTimetableSubjectwise').subscribe(
       (response: any) => {
         if (response.status) {
-          this.timetableList = this.sanitizer.bypassSecurityTrustHtml(response.resultData);
+          // this.timetableList = this.sanitizer.bypassSecurityTrustHtml(response.resultData);
+          this.timetableList = this.sanitizer.bypassSecurityTrustHtml(" <html><head></head><body>  <style  type='text/css'> /* CSS Document */ body { margin:0px;padding:0px;} .tblBorder { /*background-color: #BED9EE;*/ border: 1px solid #BED9EE;}  #ttlview td{border: 1px solid #BED9EE;} #ttlborder td{border: 1px solid #BED9EE;} /* Used for fee structure definition */  .subtitle {font-family:Arial;font-size:13px;line-height:20px;color:#121C8B;font-weight:bold;text-align: left;padding: 3px 5px;}  .title1 {font-family:Arial;font-size:15px;line-height:20px;color: #AD1010;text-align: left;padding-right: 5px;padding-left: 5px;padding-top: 3px;}  .menu1 {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 11px;line-height: 20px;font-weight: bold;color: #FFFFFF;text-decoration: none;text-align: left;padding-right: 3px;padding-left: 3px;}  .Border {background-color: #F3FBFE;border: 1px solid #BCE2F9;}  .CerTableBorder {background-color: #F3FBFE;border: 1px solid ##333333;}  .tblTitle {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;line-height: 20px;/*font-weight: bold;*/color: #000000;text-decoration: none;padding: 3px;background-color: #9CC1E2;}  .tblText {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;color: #000000;text-decoration: none;padding: 3px;background-color: #FFFFFF;text-align: left;vertical-align: top;}  .tblBreak{font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;line-height: 20px;text-decoration: none;padding: 3px;background-color: darkgray;vertical-align: middle;text-align: center;}  .tblSubstitute{font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;line-height: 20px;color: #000000;text-decoration: none;padding: 3px;background-color: aquamarine;}  .tblReserve {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;line-height: 20px;color: #000000;text-decoration: none;padding: 3px;background-color: Lavender;}  .tblStfComb {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;line-height: 20px;color: #000000;text-decoration: none;padding: 3px;background-color: Ivory;}  .tblCombCont {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;line-height: 20px;color: #000000;text-decoration: none;padding: 3px;background-color: Yellow;}  .tblCell{font-family: Goudy Old Style, Arial, Helvetica, sans-serif;font-size: 12px;color: #000000;text-decoration: none;padding: 3px;background-color: lavender;text-align: left;vertical-align: top;}  .tblHighlight{font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;text-decoration: none;padding: 3px;background-color: lightgoldenrodyellow;text-align: left;vertical-align: top;}  .tblGeneral {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;color: peru;text-decoration: none;padding: 3px;background-color: #FFFFFF;text-align: left;vertical-align: top;}  .tblName {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;font-weight: bold;color: #000000;text-decoration: none;width: 150px;float: left;}  .tblvTitle {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;line-height: 20px;font-weight: bold;color: #000000;text-decoration: none;padding: 3px;background-color: #D9E8F4;} </style> <table width=100% border=0 cellpadding=0 cellspacing=0 class=tblBorder id=ttlview><tr><td class=subtitle colspan=5>API Class A</td></tr><tr><tr><th width=6% class=tblTitle><strong>Days_Periods</strong></th> <th width=23% class=tblTitle><strong>1</strong></th> <th width=23% class=tblTitle><strong>2</strong></th> <th width=23% class=tblTitle><strong>3</strong></th> <th width=23% class=tblTitle><strong>4</strong></th> </tr><tr><td width=6% class=tblvTitle>Day 1</td> <td width=23% class=tblReserve>ENG (BKMHSS20) </td><td width=23% class=tblBreak>Break</td><td width=23% class=tblCombCont>MAT (BKMHSS140) </td><td width=23% class=tblStfComb>MAT (BKMHSS140) </td></tr><tr><td width=6% class=tblvTitle>Day 2</td> <td width=23% class=tblReserve>ENG (BKMHSS20) </td><td width=23% class=tblBreak>Break</td><td width=23% class=tblCombCont>MAT (BKMHSS140) </td><td width=23% class=tblText>TAM (BKMHSS136) </td></tr><tr><td width=6% class=tblvTitle>Day 3</td> <td width=23% class=tblReserve>ENG (BKMHSS20) </td><td width=23% class=tblBreak>Break</td><td width=23% class=tblText>&nbsp;</td><td width=23% class=tblText>MAT (BKMHSS140) </td></tr><tr><td width=6% class=tblvTitle>Day 4</td> <td width=23% class=tblReserve>ENG (BKMHSS20) </td><td width=23% class=tblBreak>Break</td><td width=23% class=tblText>&nbsp;</td><td width=23% class=tblText>MAT (BKMHSS140) </td></tr><tr><td width=6% class=tblvTitle>Day 5</td> <td width=23% class=tblReserve>ENG (BKMHSS20) </td><td width=23% class=tblBreak>Break</td><td width=23% class=tblText>&nbsp;</td><td width=23% class=tblCombCont>ENG (BKMHSS20) </td></tr></table>  </body></html>")
           this.timetableName = response.timetableName;
           this.ttlPercentage = response.ttlPercentage;
           this.loader.hide();
@@ -199,26 +201,97 @@ export class TimetablesComponent implements OnInit {
     saveAs(blob, 'Timetable.doc');
   }
   downloadAsExcel(): void {
+    // const content = document.getElementById('exceldownload'); // Adjust accordingly
+
+    // if (content instanceof HTMLElement) {
+    //   const table = content.querySelector('table');
+      
+    //   if (table) {
+    //     const wb = XLSX.utils.book_new();
+    //     const ws = XLSX.utils.table_to_sheet(table);
+    //     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    //     // Use XLSX.writeFile instead of XLSX.write
+    //     XLSX.writeFile(wb, 'Timetable.xlsx');
+    //   } else {
+    //     console.error("No table found in the 'docx' element.");
+    //   }
+    // } else {
+    //   console.error("Element with id 'docx' not found in the document.");
+    // }
+
     const content = document.getElementById('exceldownload'); // Adjust accordingly
 
     if (content instanceof HTMLElement) {
       const table = content.querySelector('table');
-      
+    
       if (table) {
-        const wb = XLSX.utils.book_new();
-        const ws = XLSX.utils.table_to_sheet(table);
-        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-        // Use XLSX.writeFile instead of XLSX.write
-        XLSX.writeFile(wb, 'Timetable.xlsx');
+        // Create a new workbook and add a worksheet
+        const workbook = new ExcelJS.Workbook();
+        const worksheet = workbook.addWorksheet('Sheet1');
+    
+        // Iterate over rows and cells to add data and apply styles
+        table.querySelectorAll('tr').forEach((row, rowIndex) => {
+          const excelRow = worksheet.getRow(rowIndex + 1); // Excel uses 1-based indexing
+    
+          row.querySelectorAll('td, th').forEach((cell, colIndex) => {
+            const bgColor = window.getComputedStyle(cell).backgroundColor;
+            const textContent = cell.textContent!.trim();
+    
+            // Add data to the cell
+            excelRow.getCell(colIndex + 1).value = textContent;
+    
+            // Parse the RGB values from the computed background color
+            const rgbValues = bgColor.match(/\d+/g);
+    
+            if (rgbValues && rgbValues.length === 3) {
+              // Apply background color to the cell
+              excelRow.getCell(colIndex + 1).fill = {
+                type: 'pattern',
+                pattern: 'solid',
+                fgColor: { argb: `FF${rgbValues.map(value => (+value).toString(16).padStart(2, '0')).join('')}` }
+              };
+            }
+              // Apply border styles to the cell
+              excelRow.border = {
+          top: { style: 'thin', color: { argb: 'ced4da' } },
+          left: { style: 'thin', color: { argb: 'ced4da' } },
+          bottom: { style: 'thin', color: { argb: 'ced4da' } },
+          right: { style: 'thin', color: { argb: 'ced4da' } }
+        };
+          });
+        });
+    
+        // Auto-adjust column widths based on content
+        worksheet.columns.forEach((column, colIndex) => {
+          let maxLength = 0;
+          worksheet.eachRow({ includeEmpty: true }, (row, rowIndex) => {
+            const cell = row.getCell(colIndex + 1);
+            if (cell && cell.text) {
+              const length = cell.text.length;
+              if (length > maxLength) {
+                maxLength = length;
+              }
+            }
+          });
+          column.width = maxLength < 10 ? 10 : maxLength + 2; // Set a minimum width
+        });
+    
+        // Save the workbook
+        workbook.xlsx.writeBuffer().then((buffer) => {
+          const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+          const link = document.createElement('a');
+          link.href = window.URL.createObjectURL(blob);
+          link.download = 'Timetable.xlsx';
+          link.click();
+        });
       } else {
-        console.error("No table found in the 'docx' element.");
+        console.error("No table found in the 'exceldownload' element.");
       }
     } else {
-      console.error("Element with id 'docx' not found in the document.");
+      console.error("Element with id 'exceldownload' not found in the document.");
     }
+  
   }
-  
-  
   
 }
